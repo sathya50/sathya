e5e2b98e9403f43216347af8e082ad9229453e6c
sathya2709 <vnsk03@gmail.com>
2025-01-25 15:08:23 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,18 +1,13 @@
 import java.io.*;
 import java.lang.*;
 import java.util.*;
-public static int reverseNumber(int num) {
-    int reversed = 0;
-    int sign = (num < 0) ? -1 : 1; // Save the sign of the number
-    num = Math.abs(num); // Work with the absolute value of the number
-    
-    while (num != 0) {
-        int digit = num % 10;
-        reversed = reversed * 10 + digit;
-        num /= 10;
+public class ReverseString {
+    public static void main(String[] args) {
+        String original = "Hello, World!";
+        StringBuilder reversed = new StringBuilder(original);
+        System.out.println("Reversed string: " + reversed.reverse().toString());
     }
-    
-    return reversed * sign; // Restore the sign of the number
 }
+
 
 
