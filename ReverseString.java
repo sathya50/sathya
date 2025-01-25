fda4f507dedf4287c35ea85e02fcdb1a038830b1
sathya50 <23124050@srcas.ac.in>
2025-01-25 14:34:05 +0530

    first commit

---
+++ ReverseString.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class ReverseString {
+
+    public static void main(String[] args) {
+        String input = "hello"; 
+        String reversed = reverseString(input);
+        System.out.println("Original: " + input);
+        System.out.println("Reversed: " + reversed);
+    }
+    public static String reverseString(String str) {
+        StringBuilder reversedString = new StringBuilder(str);
+        return reversedString.reverse().toString();
+    }
+}
